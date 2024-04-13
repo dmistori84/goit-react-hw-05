263f49cef137a279892e1cd55525fd0938dd976c
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2024-04-13 14:30:03 +0300

    add star

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
 import HomePage from "./pages/HomePage/HomePage";
 import MoviesPage from "./pages/MoviesPage/MoviesPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import MovieCast from "./components/MovieCast/MovieCast";
-import MovieReviews from "./components/MovieReviews/MovieReviews";
+// import MovieCast from "./components/MovieCast/MovieCast";
+// import MovieReviews from "./components/MovieReviews/MovieReviews";
 
 // API Key: ebb3ed9b1409881e5c8a20b8bb23e7bc
 
@@ -18,7 +18,7 @@ function App() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="/movies" element={<MoviesPage />} />
 				<Route path="*" element={<NotFoundPage />} />
-				<Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+				<Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
 				{/* <Route path="/movies/:movieId/cast" element={<MovieCast />} />
 				<Route path="/movies/:movieId/reviews" element={<MovieReviews />} /> */}
 			</Routes>
--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchMovieReviews } from "../../services/apiMovieReviews";
+
 const MovieReviews = () => {
-	return <div>MovieReviews</div>;
+	const { movieId } = useParams();
+	const [reviews, setReviews] = useState();
+	useEffect(() => {
+		async function fetchMovies() {
+			try {
+				const response = await fetchMovieReviews(movieId);
+				setReviews(response.results);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		fetchMovies();
+	}, [movieId]);
+	console.log("відповіть в ревьевсах", reviews);
+	return (
+		<div>
+			<ul>
+				{reviews &&
+					reviews.map(el => (
+						<li key={el.id}>
+							<p>Author: {el.author}</p>
+							<p>{el.content}</p>
+						</li>
+					))}
+			</ul>
+		</div>
+	);
 };
 
 export default MovieReviews;
